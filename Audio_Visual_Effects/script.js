8e2499d647f0c4fad7d3552c3a7fca20a3edab10
Daniel <daniel_ye.nz@hotmail.com>
2023-07-17 22:41:58 -0700

    add animation loop

--- Audio_Visual_Effects/script.js
+++ Audio_Visual_Effects/script.js
@@ -7,18 +7,22 @@ let canvasPosition = canvas.getBoundingClientRect();
 
 class Explosion {
     constructor(x, y){
-        this.x = x;
-        this.y = y;
         this.spriteWidth = 200;
         this.spriteHeight = 179;
-        this.width = this.spriteWidth/2;
-        this.height = this.spriteHeight/2;
+        this.width = this.spriteWidth * 0.7;
+        this.height = this.spriteHeight * 0.7;
+        this.x = x - this.width/2;
+        this.y = y - this.height/2;
         this.image = new Image();
         this.image.src = 'boom.png';
         this.frame = 0;
+        this.timer = 0;
     }
     update(){
-        this.frame++;
+        this.timer++;
+        if(this.timer % 10 === 0){
+            this.frame++;
+        } 
     }
     draw(){
         ctx.drawImage(this.image, this.spriteWidth * this.frame,
@@ -28,6 +32,18 @@ class Explosion {
 }
 
 window.addEventListener('click', function(e){
-    ctx.fillStyle = 'white';
-    ctx.fillRect(e.x - canvasPosition.left - 25, e.y - canvasPosition.top - 25, 50, 50);
+    let positionX = e.x - canvasPosition.left;
+    let positionY = e.y - canvasPosition.top;
+
+    explosions.push(new Explosion(positionX, positionY));
 })
+
+function animate(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    for(let i = 0; i < explosions.length; i++){
+        explosions[i].update();
+        explosions[i].draw();
+    }
+    requestAnimationFrame(animate);
+};
+animate();
