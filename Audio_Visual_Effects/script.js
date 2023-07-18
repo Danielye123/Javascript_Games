a9f5a9c2be28a33516dd9993b9afadb65e41c1ba
Daniel <daniel_ye.nz@hotmail.com>
2023-07-17 22:48:12 -0700

    mouse move and rotate explosion

--- Audio_Visual_Effects/script.js
+++ Audio_Visual_Effects/script.js
@@ -11,12 +11,13 @@ class Explosion {
         this.spriteHeight = 179;
         this.width = this.spriteWidth * 0.7;
         this.height = this.spriteHeight * 0.7;
-        this.x = x - this.width/2;
-        this.y = y - this.height/2;
+        this.x = x;
+        this.y = y;
         this.image = new Image();
         this.image.src = 'boom.png';
         this.frame = 0;
         this.timer = 0;
+        this.angle = Math.random() * 6.2;
     }
     update(){
         this.timer++;
@@ -25,13 +26,21 @@ class Explosion {
         } 
     }
     draw(){
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle)
         ctx.drawImage(this.image, this.spriteWidth * this.frame,
             0, this.spriteWidth, this.spriteHeight,
-            this.x, this.y, this.width, this.height);
+            0 - this.width/2, 0 - this.width/2, this.width, this.height);
+        ctx.restore();
     }
 }
 
 window.addEventListener('click', function(e){
+    createAnimation(e);
+})
+
+window.addEventListener('mousemove', function(e){
     createAnimation(e);
 })
 
